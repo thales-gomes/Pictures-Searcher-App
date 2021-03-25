41b32c94355ee01f15d813fdf9bce28b5cd64cb7
Thales Gomes <thalessud@gmail.com>
2021-03-25 20:02:14 +0000

    setSpans helper added to ImageCard component

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './ImageList.css'
+import ImageCard from'./ImageCard'
 
 export default function ImageList({ images }) {
-  const imageComponent = images.map(({id, urls, alt_description}) => {
-    return <img key={id} src={urls.regular} alt={alt_description}/>
+  const imageComponent = images.map(image => {
+    return <ImageCard key={image.id} image={image}/>
   })
 
   return <div className="image-list">{imageComponent}</div>
