8a88e3a0f13b0917d020756b105122557584d892
Thales Gomes <thalessud@gmail.com>
2021-03-25 20:28:29 +0000

    finish styling

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -3,21 +3,24 @@ import React from "react";
 class ImageCard extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {spans: 0}
     this.imageRef = React.createRef(); // React Ref system to access element info without the need of document selectors
   }
 
   componentDidMount() {
-    this.imageRef.current.addEventListener("load", this.setSpans) // Awaits Image loads to access Height
+    this.imageRef.current.addEventListener("load", this.setSpans) // Awaits Image loads so callback can access info
   }
 
   setSpans = () => {
-    console.log(this.imageRef.current.clientHeight)
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10 + 1)   // Grid row heigth, up rounded to comport image 
+    this.setState({ spans })
   }
 
 	render() {
 		const { description, urls } = this.props.image;
 		return (
-			<div>
+			<div style={{gridRowEnd: `span ${this.state.spans}`}} >
 				<img ref={this.imageRef} alt={description} src={urls.regular} />
 			</div>
 		);
