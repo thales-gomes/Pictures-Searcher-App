f3575c00ad4ae6e9a2c7d3aca9b53d3708ee46d8
Thales Gomes <thalessud@gmail.com>
2021-03-25 19:22:41 +0000

    css grid added on ImageList.css

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
+import './ImageList.css'
 
 export default function ImageList({ images }) {
   const imageComponent = images.map(({id, urls, alt_description}) => {
     return <img key={id} src={urls.regular} alt={alt_description}/>
   })
 
-  return (
-    <div>
-      {imageComponent}
-    </div>
-  )
+  return <div className="image-list">{imageComponent}</div>
 }
